319893dafc5191647c1fcf832daf4718ec9b4ad1
Timofei <trusanov@1-E-3.42heilbronn.de>
2023-04-20 11:07:15 +0200

    test.sh path fix

--- c01/test.sh
+++ c01/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-norminette -R CheckForbiddenSourceHeader ../ex00 ../ex01 ../ex02 ../ex03 ../ex04 ../ex05 ../ex06 ../ex07 ../ex08
+norminette -R CheckForbiddenSourceHeader ../../ex00 ../../ex01 ../../ex02 ../../ex03 ../../ex04 ../../ex05 ../../ex06 ../../ex07 ../../ex08
 cc -Wall -Wextra -Werror test.c
 ./a.out
