4ea9a95114432b0388d991bda4c594d7e39e05a1
Timofei <trusanov@1-E-3.42heilbronn.de>
2023-04-20 15:25:37 +0200

    Revert "c01 fix"
    This reverts commit 9d4e41aed770c8325448599aface629b25df2f39.

--- c01/test.c
+++ c01/test.c
@@ -1,13 +1,13 @@
 #include <assert.h>
-#include "ex00/ft_ft.c"
-#include "ex01/ft_ultimate_ft.c"
-#include "ex02/ft_swap.c"
-#include "ex03/ft_div_mod.c"
-#include "ex04/ft_ultimate_div_mod.c"
-#include "ex05/ft_putstr.c"
-#include "ex06/ft_strlen.c"
-#include "ex07/ft_rev_int_tab.c"
-#include "ex08/ft_sort_int_tab.c"
+#include "../../ex00/ft_ft.c"
+#include "../../ex01/ft_ultimate_ft.c"
+#include "../../ex02/ft_swap.c"
+#include "../../ex03/ft_div_mod.c"
+#include "../../ex04/ft_ultimate_div_mod.c"
+#include "../../ex05/ft_putstr.c"
+#include "../../ex06/ft_strlen.c"
+#include "../../ex07/ft_rev_int_tab.c"
+#include "../../ex08/ft_sort_int_tab.c"
 
 void test0()
 {
